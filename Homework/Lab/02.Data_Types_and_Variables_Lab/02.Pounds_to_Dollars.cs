b30118dc71d0479a2dfb912d1f7b606269a622d1
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-05-28 17:07:18 +0300

    Create 02.Pounds_to_Dollars.cs

---
+++ Homework/Lab/02.Data_Types_and_Variables_Lab/02.Pounds_to_Dollars.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace _02.Pounds_to_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal GBP = decimal.Parse(Console.ReadLine());
+            decimal USD = GBP * 1.31m;
+            Console.WriteLine($"{USD:f3}");
+        }
+    }
+}
