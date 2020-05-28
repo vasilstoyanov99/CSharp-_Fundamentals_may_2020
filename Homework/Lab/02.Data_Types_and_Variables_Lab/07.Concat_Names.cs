869834a34c9c545a090e4f6ec973eced6ac1b153
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-05-28 17:08:15 +0300

    Create 07.Concat_Names.cs

---
+++ Homework/Lab/02.Data_Types_and_Variables_Lab/07.Concat_Names.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace _07.Concat_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            Console.WriteLine($"{firstName}{delimiter}{lastName}");
+        }
+    }
+}
