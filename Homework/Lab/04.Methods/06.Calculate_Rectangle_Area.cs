4e86528a0c08e1b128606e27f7063d2e058e0d52
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-06-13 21:19:07 +0300

    Create 06.Calculate_Rectangle_Area.cs

---
+++ Homework/Lab/04.Methods/06.Calculate_Rectangle_Area.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace _06.Calculate_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine(GetRectangleArea(width, height));
+        }
+
+        static double GetRectangleArea(double width, double height)
+        {
+            return width * height;
+        }
+    }
+}
